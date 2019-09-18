b6afe8aba9985ee1f8f360b63321234f32f84ee2
Saeed Khaki <saeedkhaki1992@gmail.com>
2019-09-18 14:37:07 -0500

    Update CNN_combined_Drought_Heat.py

--- CNN_combined_Drought_Heat.py
+++ CNN_combined_Drought_Heat.py
@@ -488,9 +488,18 @@ layers_dh = [10, 1]
 
 is_training = True
 
-rmse_tr, rmse_te = main_function(H_X, D1_X, Y, layers_dh, batch_size_tr, batch_size_te, learning_rate, max_it, p)
+rmse_te_all,rmse_tr_all = main_function(H_X, D1_X, Y, layers_dh, batch_size_tr, batch_size_te, learning_rate, max_it, p)
+print('train')
+print(rmse_tr_all)
+print('test')
+print(rmse_te_all)
 
-print('train RMSE is %f and test RMSE is %f ' % (rmse_tr, rmse_te))
+
+
+print('train RMSE is %f and test RMSE is %f ' %(np.mean(rmse_tr_all),np.mean(rmse_te_all)))
+
+
+
 
 
 
